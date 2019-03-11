26b7e8abec4bfbe93ad37225672adbf636193822
Jonas <erthaljonas@gmail.com>
2019-03-10 21:32:13 -0400

    Fixed map centering issue

--- render.py
+++ render.py
@@ -2,11 +2,12 @@ import pygame
 from pygame.locals import *
 
 def render_map(DISPLAYSURF, tilemap, colors, SCREENWIDTH, SCREENHEIGHT, MAPWIDTH, MAPHEIGHT, TILESIZE):
-    start_X = (SCREENWIDTH / 2) - (MAPWIDTH / 2)
-    start_Y = (SCREENHEIGHT / 2) - (MAPHEIGHT / 2)
+    start_X = (SCREENWIDTH / 2) - ((MAPWIDTH * TILESIZE) / 2)
+    start_Y = (SCREENHEIGHT / 2) - ((MAPHEIGHT * TILESIZE) / 2)
+    
     for row in range(MAPHEIGHT):
         for col in range(MAPWIDTH):
-            dist_X = start_X + (row * TILESIZE)
-            dist_Y = start_Y + (col * TILESIZE)
+            dist_X = start_X + (col * TILESIZE)
+            dist_Y = start_Y + (row * TILESIZE)
             current_tile = tilemap[row][col]
-            pygame.draw.rect(DISPLAYSURF, colors[current_tile], (dist_Y, dist_X, TILESIZE, TILESIZE))
+            pygame.draw.rect(DISPLAYSURF, colors[current_tile], (dist_X, dist_Y, TILESIZE, TILESIZE))
